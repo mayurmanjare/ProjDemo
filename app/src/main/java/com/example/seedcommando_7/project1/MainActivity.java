ac78dccc622df26478a46491dc231c6cae89f6ef
mayurmanjare <mayurmanjare4232@gmail.com>
2016-12-14 15:45:23 +0530

    Tost Hello

--- app/src/main/java/com/example/seedcommando_7/project1/MainActivity.java
+++ app/src/main/java/com/example/seedcommando_7/project1/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this,"Rashmi",Toast.LENGTH_LONG).show();
+        Toast.makeText(this,"Hello",Toast.LENGTH_LONG).show();
     }
 
 }
